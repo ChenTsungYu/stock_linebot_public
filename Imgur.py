cc775fbae5a002850448650291fcb59eb0299a35
TSUNG YU CHEN <46296838+ChenTsungYu@users.noreply.github.com>
2020-11-25 23:54:21 +0800

    Update Imgur.py

--- Imgur.py
+++ Imgur.py
@@ -1,36 +1,36 @@
 '''
-上傳圖片
+Upload pics
 '''
 import matplotlib
 matplotlib.use('Agg')
 import datetime
 from imgurpython import ImgurClient
-client_id = '07f1098efd49fcb'
-client_secret = 'c802e2b89ad38a83341f53148a41058c65a65d42'
-album_id = '817ZzND'
-access_token = '0409f8c9d920da65fc8bb9c3ec5b029c4dc4869c'
-refresh_token = 'dd279237af22c09c879a84105e209e13bff5e227'
+client_id = 'Your imgur client id'
+client_secret = 'Your imgur client secret'
+album_id = 'Your imgur album id'
+access_token = 'Your imgur access token'
+refresh_token = 'Your imgur refresh token'
 
 def showImgur(fileName):
-        # 連接imgur
+        # connect imgur
         client= ImgurClient(client_id, client_secret, access_token, refresh_token)
     
-        # 連接需要的參數
+        # params
         config = {
-            'album': album_id, # 相簿名稱
-            'name': fileName, # 圖片名稱
-            'title': fileName, # 圖片標題
-            'description': str(datetime.date.today()) # 備註，這邊打日期時間
+            'album': album_id, # album name
+            'name': fileName, # pics name
+            'title': fileName, # pics title
+            'description': str(datetime.date.today())
             }
         
-        # 開始上傳檔案
+        # Upload file
         try:
             print("[log:INFO]Uploading image... ")
             imgurl = client.upload_from_path(fileName+'.png', config=config, anon=False)['link']
             #string to dict
             print("[log:INFO]Done upload. ")
         except :
-            # 如果失敗回傳"失敗"這張圖
+            # if faild to upload
             imgurl = 'https://i.imgur.com/RFmkvQX.jpg'
             print("[log:ERROR]Unable upload ! ")
             
